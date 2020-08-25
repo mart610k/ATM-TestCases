67806a548a8d02885182c693152e083be898dd71
mart610k <martin-baage@hotmail.com>
2020-08-25 08:53:20 +0200

    Intital test for confirming test

---
+++ ATM.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using ATM_TestCases;
+using NUnit.Framework;
+
+namespace ATM.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

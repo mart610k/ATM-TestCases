45e4859600b418353479f2c81d00a4e89afac083
mart610k <martin-baage@hotmail.com>
2020-08-25 15:11:58 +0200

    ATM Stubs Added.

---
+++ ATM-TestCases/ATM/ATM.cs
@@ -0,0 +1,37 @@
+﻿using ATM_TestCases.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_TestCases.ATM
+{
+    class ATM : IATM
+    {
+        public ICard CardInserted => throw new NotImplementedException();
+
+        public decimal GetAccountBalance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetPinCodeFromUser(string pinInserted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertCard(ICard cardInserted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICard RemoveCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal RemoveFromBalance(decimal amountToRemove)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ATM-TestCases/ATM/IATM.cs
@@ -0,0 +1,22 @@
+﻿using ATM_TestCases.Cards;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_TestCases.ATM
+{
+    interface IATM
+    {
+        ICard CardInserted { get; }
+
+        bool GetPinCodeFromUser(string pinInserted);
+
+        decimal GetAccountBalance();
+
+        decimal RemoveFromBalance(decimal amountToRemove);
+
+        bool InsertCard(ICard cardInserted);
+
+        ICard RemoveCard();
+    }
+}

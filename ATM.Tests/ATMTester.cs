8c696107f1a885f4c0c06ed26d9f152facc68ee2
mart610k <martin-baage@hotmail.com>
2020-08-25 15:18:14 +0200

    Added first ATM test. and removed starating Unit test class

---
+++ ATM.Tests/ATMTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ATM_TestCases.ATMClasses;
+
+namespace ATM.Tests
+{
+    class ATMTester
+    {
+        [Test]
+        public void GetNullFromCardInserted()
+        {
+            IATM atm = new ATM_TestCases.ATMClasses.ATM();
+
+
+            Assert.IsNull(atm.CardInserted);
+        }
+    }
+}

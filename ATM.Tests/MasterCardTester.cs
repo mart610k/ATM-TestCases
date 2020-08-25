714dbc5926efab59f2bf060faa8f9bd899f00dbe
mart610k <martin-baage@hotmail.com>
2020-08-25 09:20:06 +0200

    Added Test for checking Constructor does not throw an execption

---
+++ ATM.Tests/MasterCardTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ATM_TestCases.Cards;
+
+namespace ATM.Tests
+{
+    class MasterCardTester
+    {
+        [Test]
+        public void ConstructorImplemented()
+        {
+            ICard card;
+            Assert.DoesNotThrow(() => card = new MasterCard("5555555555555555",1000M,4322,new DateTime(2030,10,5))) ;
+        }
+
+
+    }
+}
